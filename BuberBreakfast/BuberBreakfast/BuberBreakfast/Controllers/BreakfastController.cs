dc72d17dbfbce98f0a06395a554a5c3fcd74e5e1
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-15 16:09:20 -0400

    add get and post

--- BuberBreakfast/BuberBreakfast/BuberBreakfast/Controllers/BreakfastController.cs
+++ BuberBreakfast/BuberBreakfast/BuberBreakfast/Controllers/BreakfastController.cs
@@ -1,9 +1,41 @@
+using BuberBreakfast.Contracts.Breakfast;
+using BuberBreakfast.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
-public class BreakfastController: ControllerBase {
-    [HttpPost("/breakfast")]
-    public IActionResult CreateBreakfast(CreateBreakfastRequest request) {
-        return Ok();
+[Route("[controller]")]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        var breakfast = new Breakfast(
+            Guid.NewGuid(),
+            request.Name, 
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet
+            );
+        return Ok(request);
+    }
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(id);
+    }
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
     }
 }
