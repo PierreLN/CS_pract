91d661ba459e619e4dfb520b6f9b0eade488ac7e
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-16 13:30:25 -0400

    add get and post + migration

--- Contacts/PhoneBook/PhoneBook/Controllers/PhoneController.cs
+++ Contacts/PhoneBook/PhoneBook/Controllers/PhoneController.cs
@@ -15,5 +15,28 @@ namespace PhoneBook.Controllers
 
         [HttpGet]
         public async Task<IEnumerable<Phone>> Get() => await _context.Phones.ToListAsync();
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Phone), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var phone = await _context.Phones.FindAsync(id);
+            return phone == null? NotFound() : Ok(phone);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Phone phone)
+        {
+            await _context.Phones.AddAsync(phone);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new {id = phone.Id}, phone);   
+        }
+
+
     }
 }
